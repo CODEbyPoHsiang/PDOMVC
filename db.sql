7a39d6bbdf5cfa8ce17f054d1568836f8c4b3938
CODEbyPoHsiang <52902326+CODEbyPoHsiang@users.noreply.github.com>
2020-05-29 00:23:00 +0800

    Update db.sql
    更新sql

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2020-05-28 16:45:50
+-- 產生時間： 2020-05-28 18:21:38
 -- 伺服器版本： 10.4.11-MariaDB
 -- PHP 版本： 7.4.4
 
@@ -44,6 +44,27 @@ CREATE TABLE `member` (
 INSERT INTO `member` (`id`, `name`, `phone`, `ename`, `email`, `sex`, `city`, `township`, `postcode`, `address`, `notes`, `created_at`, `updated_at`) VALUES
 (2, 'ww', 'rr', 'www', 'qqqqqqqqqqqqqqqq', '', '', 'qqqqqqqqqqqqq', '', 'qqqqqqqqqqqqqq', '', '2020-05-21 03:23:45', '2020-05-21 03:25:38');
 
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `music`
+--
+
+CREATE TABLE `music` (
+  `id` int(12) NOT NULL,
+  `name` varchar(65) CHARACTER SET utf8mb4 NOT NULL,
+  `phone` varchar(65) CHARACTER SET utf8mb4 NOT NULL,
+  `ename` varchar(200) CHARACTER SET utf8mb4 NOT NULL,
+  `email` varchar(200) CHARACTER SET utf8mb4 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 傾印資料表的資料 `music`
+--
+
+INSERT INTO `music` (`id`, `name`, `phone`, `ename`, `email`) VALUES
+(17, '阿明', '0919000999', 'jay', '0939cool@gmail.com');
+
 --
 -- 已傾印資料表的索引
 --
@@ -55,6 +76,12 @@ ALTER TABLE `member`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `music`
+--
+ALTER TABLE `music`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
 
@@ -63,4 +90,10 @@ ALTER TABLE `member`
 --
 ALTER TABLE `member`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `music`
+--
+ALTER TABLE `music`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
